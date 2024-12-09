946120b7e62a1f61ae1da2864248e13c540375eb
Cladkoewka <codewarriorms@gmail.com>
2024-12-09 15:42:12 +0300

    Fixes

--- OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
+++ OrderService/obj/Debug/net8.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cba4222f376471fd54e0fe2915c918803c717c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac5af7a9db66f270b2168e96e0803791c79d2042")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
